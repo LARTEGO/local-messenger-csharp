7009dbc8ce08b8b338ce81ef71670d8f59a643a8
LARTEGO <148080342+LARTEGO@users.noreply.github.com>
2025-11-13 19:27:36 +0400

    add project files

---
+++ Models/Message.cs
@@ -0,0 +1,11 @@
+namespace LocalMessenger.Models
+{
+
+public class Message
+{
+    public int Id { get; set; }
+    public string UserName { get; set; }
+    public string Text { get; set; }
+    public DateTime TimeStamp { get; set; }
+}
+}
