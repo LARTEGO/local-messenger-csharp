6315d68f3af3344efd1d234b9a5d9846da4b415e
LARTEGO <art.shadrin.266@yandex.ru>
2025-11-17 19:28:08 +0400

    SignalR
    dotnet add package Mocrosoft.AspNetCore.SignalR

--- obj/Debug/net8.0/LocalMessenger.AssemblyInfo.cs
+++ obj/Debug/net8.0/LocalMessenger.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LocalMessenger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+989f06406c01aa6d9be8c02f21fe3a862674c3f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a964e46894c8e201c34912a23edca04371ef89e")]
 [assembly: System.Reflection.AssemblyProductAttribute("LocalMessenger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LocalMessenger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
