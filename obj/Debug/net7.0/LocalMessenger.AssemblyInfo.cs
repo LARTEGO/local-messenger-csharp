6be98e6dd4d84481ce2d6ad07f8cf4d407fcee06
LARTEGO <art.shadrin.266@yandex.ru>
2026-01-12 19:50:53 +0400

    rerework
    оно наконец то работает

--- obj/Debug/net7.0/LocalMessenger.AssemblyInfo.cs
+++ obj/Debug/net7.0/LocalMessenger.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LocalMessenger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92ee2a0b5a096374405e360af436952bf5e22c01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LocalMessenger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LocalMessenger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
